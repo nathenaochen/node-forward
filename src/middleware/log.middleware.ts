72eb411e8d345fa83ca8960232116be70903842f
Nathen <13147177623@163.com>
2020-09-14 18:53:51 +0800

    完善中间件，增加参数，转发ip等判断并给出错误提示

--- src/middleware/log.middleware.ts
+++ src/middleware/log.middleware.ts
@@ -1,18 +1,31 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import {ConfService} from '../conf/conf.service';
+import {resData} from '../dto/res.dto';
 
 //类中间件 -- 只能在module中通过consumer.apply()的方式实现
 @Injectable()
 export class LogMiddleware implements NestMiddleware {
+  constructor(private readonly configService: ConfService){}
   async use(req: Request |any, res: any, next: () => void) {
-    // console.log('middleware',req.url,req.body,req.query,req.method);
-    //如果是接口转发时，判断是否又传type和serviceName两个必传参数
-    console.log();
-    if(req.url.split('?')[0] == '/api'){
-      let params = req.method == 'POST' ? req.body : req.query;
-      if(!params.snType || !params.serviceName){
-        throw new Error('snType or serviiceName is required');
+    try{
+      // console.log('middleware',req.url,req.body,req.query,req.method);
+      //如果是接口转发时，判断是否又传type和serviceName两个必传参数
+      if(req.url.split('?')[0] == '/api'){
+        let params = req.method == 'POST' ? req.body : req.query;
+        if(!params.snType || !params.serviceName){
+          throw new Error('snType or serviceName is required');
+        }
+        let serviceIp = this.configService.getValue(`${params['snType']}ServiceIp`);
+        let servicePort = this.configService.getValue(`${params['snType']}ServicePort`);
+        let serviceUri = this.configService.getValue(params['serviceName']);
+        //检查是否能找到对应的转达ip，端口或者uri
+        if(!serviceIp || !servicePort || !serviceUri){
+          throw new Error('cant find serviceIp,servicePort,serviceUri, please checkout your params or config file');
+        }
       }
+      next();
+    }catch(err){
+      res.send(resData.fail({},err.message))
     }
-    next();
   }
 }
